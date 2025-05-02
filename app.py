12e9164319fb5686e44e77c335df05e6c360c3c4
ibeiwasibeen <zlata.oleinichenko@gmail.com>
2025-05-01 17:34:18 -0700

    Added logging for IP addresses

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
-from waitress import serve  # Импортируем serve для запуска через waitress
+from waitress import serve 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    ip_address = request.remote_addr
+    print(f"Received request from: {ip_address}")  # Выводим IP-адрес в консоль
+    
+    with open("ip_log.txt", "a") as log_file:
+        log_file.write(f"{datetime.now()}: {ip_address}\n")
+    
     return render_template("index.html")
 
 if __name__ == "__main__":
     print("Starting the server...")
     serve(app, host="0.0.0.0", port=8000)
+
